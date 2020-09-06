5e2e3b922b0ce4aef6c9587f6856f43ec598152a
Luis Miguel Fernandez <luismiguel.fernandez@viseo.com>
2020-09-06 21:00:29 +0200

    Fixed project name.

--- src/azure-devops-cloning-repositories.tf
+++ src/azure-devops-cloning-repositories.tf
@@ -41,7 +41,7 @@ resource "null_resource" "clone_infrastructure_repo" {
     provisioner "local-exec" {
         #command = "git clone --mirror https://github.com/lmfespinosa/test-repo.git && cd test-repo.git && git remote add ${azuredevops_git_repository.repository.remote_url}.gt &&  git remote add origin ${azuredevops_git_repository.repository.remote_url}.git  && git push --force --set-upstream origin master ${azuredevops_git_repository.repository.remote_url}.git"
         #command = "git clone ${azuredevops_git_repository.repository.remote_url} && "
-        command = "git clone https://github.com/lmfespinosa/DevOps-MEng-UNIR-MPIS.InfrastructureDevOps && cd DevOps-MEng-UNIR-MPIS.InfrastructureDevOps && git branch -a && git checkout master && git fetch --tags && git tag && git branch -a && git remote rm origin && git remote add origin ${var.org_service_url}/${var.Project-Name}/_git/MPIS.InfrastructureDevOps && git pull --allow-unrelated-histories origin master && git push --set-upstream origin master && cd .. && rmdir /s /Q DevOps-MEng-UNIR-MPIS.InfrastructureDevOps"
+        command = "git clone https://github.com/lmfespinosa/DevOps-MEng-UNIR-MPIS.InfrastructureDevOps && cd DevOps-MEng-UNIR-MPIS.InfrastructureDevOps && git branch -a && git checkout master && git fetch --tags && git tag && git branch -a && git remote rm origin && git remote add origin ${var.org_service_url}/${data.azuredevops_project.project"}/_git/MPIS.InfrastructureDevOps && git pull --allow-unrelated-histories origin master && git push --set-upstream origin master && cd .. && rmdir /s /Q DevOps-MEng-UNIR-MPIS.InfrastructureDevOps"
     }
 
     depends_on = [azuredevops_project.project]
